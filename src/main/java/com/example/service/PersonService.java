9054bc715065779c92cfca46a00970b7452da2d7
OUSSAMAFALLAH <49482664+OUSSAMAFALLAH@users.noreply.github.com>
2021-01-28 16:33:49 +0100

    new updating for webhook

--- src/main/java/com/example/service/PersonService.java
+++ src/main/java/com/example/service/PersonService.java
@@ -40,6 +40,13 @@ public class PersonService {
 		p.setLastName("Mahmoud");
 		persons.add(p);
 		
+		p = new Person();
+		p.setId("5");
+		p.setAge(50);
+		p.setFirstName("AbdulHalim");
+		p.setLastName("Webhook");
+		persons.add(p);
+		
 	}
 	public Person getPerson(String id) {
 		for(Person person:persons) {
